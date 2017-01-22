3361edc340b3520ec38a7e2792290f81fb61158f
Takehana Yukinobu <takehana@takehana-macbook-air.local>
2017-01-23 06:18:12 +0900

    Initial Commit

---
+++ Mycalc3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mycalc3
+//
+//  Created by Takehana Yukinobu on 2017/01/23.
+//  Copyright © 2017年 Takehana Yukinobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
